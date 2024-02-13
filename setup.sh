82587ca6e2006c1c05a9e28d68f8fce93d07fd4d
AL KAISI Ibrahim <ibrahimalkaisi.iut@gmail.com>
2024-02-13 12:34:58 +0100

    v1.1

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Exécutez docker-compose
-docker-compose up -d
+./purge.sh
 
 # Créez un environnement virtuel
 python3 -m venv env_vir
 
 # Activez l'environnement virtuel
 source env_vir/bin/activate
+
+# Exécutez docker-compose
+docker-compose up -d
+
+pip install Flask
+
+pip install mysql-connector
 
 # Exécutez l'application Flask
 python3 python/app.py
