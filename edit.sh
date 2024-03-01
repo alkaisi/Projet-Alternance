7c8d7512e1923bd36c2fd7ad3e9819886e28bf83
AL KAISI <ibrahimalkaisi.iut@gmail.com>
2024-03-01 15:44:35 +0100

    v.1

--- edit.sh
+++ edit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Créez un environnement virtuel
-python3 -m venv env_vir
+# python3 -m venv env_vir
 
 # Activez l'environnement virtuel
 source env_vir/bin/activate
@@ -9,9 +9,11 @@ source env_vir/bin/activate
 # Exécutez docker-compose
 docker-compose up -d
 
-pip install Flask
+# pip install Flask
 
-pip install mysql-connector
+# pip install mysql-connector
+
+# pip install mysql-connector-python
 
 # Exécutez l'application Flask
 python3 python/edit/app.py
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 ./purge.sh
 
 # Créez un environnement virtuel
-python3 -m venv env_vir
+# python3 -m venv env_vir
 
 # Activez l'environnement virtuel
 source env_vir/bin/activate
@@ -11,9 +11,11 @@ source env_vir/bin/activate
 # Exécutez docker-compose
 docker-compose up -d
 
-pip install Flask
+# pip install Flask
 
-pip install mysql-connector
+# pip install mysql-connector
+
+# pip install mysql-connector-python
 
 # Exécutez l'application Flask
 python3 python/app.py
